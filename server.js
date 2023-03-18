618092f8bd92cbbd0f68b309175b65eec37408c4
codernikhil12 <108299232+codernikhil12@users.noreply.github.com>
2023-03-18 12:00:44 +0530

    updated

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan(`dev`));
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
@@ -31,8 +31,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/bulid/index.html"));
+pp.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 8000;
